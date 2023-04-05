c525f0498957dc7a82f8257fdf6ba078826eab2c
Gaole Dai <gaole001@e.ntu.edu.sg>
2023-04-05 11:37:36 +0800

    improve attention

--- run.sh
+++ run.sh
@@ -3,4 +3,4 @@
 # python main.py -name 'attention' -lr 0.005 -e 75 &
 # python main.py -name 'attention' -lr 0.005 -e 100 &
 
-python main.py -name 'biLSTM' -lr 0.01 -e 30
+python main.py -name 'attention' -lr 0.01 -e 30
